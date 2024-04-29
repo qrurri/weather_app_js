3a4d9327f7a79879acc19a47d4e5743751bb216b
qrurri <rncjfl1894@gmail.com>
2024-04-29 12:51:14 +0900

    0429

---
+++ js/app.js
@@ -0,0 +1,12 @@
+/**
+ *
+ * @param {NodeList} elmts selected elements from html to add events // NodeList : html에 있는 각각의 노드 요소들을 저장하라는 뜻.
+ * @param {string} event event type e.g. 'click', 'mouseenter'
+ * @param {function} callback callback function to be executed when event is triggered
+ */
+
+export function eventOnElmts(elmts, event, callback) {
+  for (const elmt of elmts) {
+    elmt.addEventListener(event, callback);
+  }
+} // 여기까지 함수를 정의한거고 이제 이 함수를 작동시켜보자. main.js에서...
